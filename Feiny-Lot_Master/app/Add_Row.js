1d26863c2f77b49335ef836267903d08c21a1ecb
ArjunDhurai <133147912+ArjunDhurai@users.noreply.github.com>
2026-05-08 17:47:23 +0530

    feat: implement add row functions for jewellery details and update button actions in HTML

--- Feiny-Lot_Master/app/Add_Row.js
+++ Feiny-Lot_Master/app/Add_Row.js
@@ -47,3 +47,103 @@ function addJewellery1Row() {
   loadPurityLookup();
   loadUnitLookup();
 }
+
+// Add Row Function for Jewellery 2 – Diamond Details
+
+function addJewellery2Row() {
+  console.log("Adding Jewellery 2 row...");
+  const tbody = document.getElementById("jewel2Body");
+
+  const newRow = document.createElement("tr");
+  newRow.className = "partner-row";
+  newRow.innerHTML = `
+              
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input type="number" /></td>
+                  <td><input type="number" /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><textarea></textarea></td>
+                
+        `;
+  tbody.appendChild(newRow);
+}
+
+// Add Row Function for Jewellery 2 – Diamond Details
+function addJewellery3Row() {
+  console.log("Adding Jewellery 3 row...");
+  const tbody = document.getElementById("jewel3Body");
+
+  const newRow = document.createElement("tr");
+  newRow.className = "partner-row";
+  newRow.innerHTML = `
+              
+                  <td><input /></td>
+                  <td><select></select></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input type="checkbox" /></td>
+                  <td><input type="checkbox" /></td>
+                  <td><textarea></textarea></td>
+                
+        `;
+  tbody.appendChild(newRow);
+}
+
+// Add Row Function for Jewellery 2 – Diamond Details
+function addJewellery4Row() {
+  console.log("Adding Jewellery 4 row...");
+  const tbody = document.getElementById("jewel4Body");
+
+  const newRow = document.createElement("tr");
+  newRow.className = "partner-row";
+  newRow.innerHTML = `
+              
+                  <td><input /></td>
+                  <td><textarea></textarea></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input type="checkbox" /></td>
+                  <td><input /></td>
+                
+        `;
+  tbody.appendChild(newRow);
+}
+
+// Add Row Function for Jewellery Partnership Details
+function addJewellerypartnershipRow() {
+  console.log("Adding Jewellery 5 row...");
+  const tbody = document.getElementById("subform-body");
+
+  const newRow = document.createElement("tr");
+  newRow.className = "partner-row";
+  newRow.innerHTML = `
+              <td>
+                                        <select id="select_contact">
+                        <option value="">Select Contact</option>
+                      </select>
+                  </td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input /></td>
+                  <td><input type="checkbox" /></td>
+                  <td><textarea></textarea></td>
+                
+        `;
+  tbody.appendChild(newRow);
+  loadContactLookup();
+}
